e96ef1501f77d9c9edbfcb49546630a6767ec675
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-01-17 18:12:41 +0100

    Ran on Freya

--- AthenaK_legacy/kh_build/src/submit.sh
+++ AthenaK_legacy/kh_build/src/submit.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=athena_cpu
+#SBATCH --job-name=athena
 #SBATCH --output=athena.out
 #SBATCH --error=athena.err
-#SBATCH --partition=all_srv
+#SBATCH --partition=p.test
+#SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --time=2-00:00:00
+#SBATCH --cpus-per-task=40
+#SBATCH --time=00:30:00
 
-# Optional: load any needed modules
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+module purge
+module load gcc/10
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
-./athena -i kh_cooling_pcunits.athinput -d rh512_256/
-# ./athena -i kh_cooling_pcunits.athinput -d rch512_256/ -r rh512_256/rst/KH.00005.rst
+
+export PYTHONPATH=$PWD/python:$PYTHONPATH
+
+# ---- main run ----
+# ./athena -i kh_cooling_pcunits.athinput -d rc512_256/
+
+# ---- restart run ----
+./athena -i kh_cooling_pcunits.athinput -d c512_256/ -r rc512_256/rst/KH.00005.rst
