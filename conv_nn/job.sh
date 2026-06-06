11203fcf1902900512d92e54a61226695a179870
Dipayan Datta <dipda@freya01.bc.mpcdf.mpg.de>
2026-06-06 14:56:57 +0200

    unnorm dens run

--- conv_nn/job.sh
+++ conv_nn/job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=cnn_train
-#SBATCH --partition=p.gpu
-#SBATCH --gres=gpu:2
-# #SBATCH --partition=p.24h
+# #SBATCH --partition=p.gpu
+# #SBATCH --gres=gpu:2
+#SBATCH --partition=p.test
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=40
-#SBATCH --time=03:00:00
+# #SBATCH --time=03:00:00
 #SBATCH --output=job.log
 #SBATCH --error=job.err
 
